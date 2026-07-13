cfcbdf6d18777929ee2f3238018aa264200de021
Amit Tewatia <tewatiaamit01@gmail.com>
2026-07-13 11:56:16 +0530

    added clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,16 @@ export default function RootLayout({
             gtag('config', 'AW-18305531844');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xloba09jvx");
+          `}
+        </Script>
       </head>
       <body className={`${bricolage.variable} ${instrument.variable}`}>
         {children}
